d8ba85c4b988aa60bed03d5c35a1167c471222fe
d4vvv <kacper.godula99@gmail.com>
2023-03-28 22:15:54 +0200

    add pride flag

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,150 +1,161 @@
-import cs from "classnames";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import logo from "../../public/img/logo.png";
-import headerImg from "../../public/img/undraw_augmented_reality_heuy.svg";
-import { Flags } from "./Flags";
+import cs from 'classnames'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import logo from '../../public/img/logo.png'
+import headerImg from '../../public/img/undraw_augmented_reality_heuy.svg'
+import { Flags } from './Flags'
+import styles from './Header.module.css'
 
 export const Header: React.FC = () => {
-  const [isMenuToggled, setMenuToggled] = useState(false);
-  const router = useRouter();
+  const [isMenuToggled, setMenuToggled] = useState(false)
+  const [isRainbowActive, setRainbowActive] = useState(false)
+  const router = useRouter()
+
+  const onClickPride = () => {
+    setRainbowActive(true)
+    setTimeout(() => setRainbowActive(false), 10000)
+  }
 
   const renderButton = () => {
-    if (!router.pathname.includes("contact")) {
+    if (!router.pathname.includes('contact')) {
       return (
         <button
-          type="button"
-          className="font-medium font-Lexend border-[#f23cfa] border-4 h-10 rounded-full text-[#f23cfa] w-1/2 hover:bg-[#f23cfa] hover:text-white transition-all duration-300"
-          name="button"
+          type='button'
+          className='font-medium font-Lexend border-[#f23cfa] border-4 h-10 rounded-full text-[#f23cfa] w-1/2 hover:bg-[#f23cfa] hover:text-white transition-all duration-300'
+          name='button'
           onClick={() => {
             document
-              .querySelector("#about")
-              ?.scrollIntoView({ behavior: "smooth" });
-            setMenuToggled(false);
+              .querySelector('#about')
+              ?.scrollIntoView({ behavior: 'smooth' })
+            setMenuToggled(false)
           }}
         >
           Show More
         </button>
-      );
+      )
     }
-    return null;
-  };
+    return null
+  }
 
   const renderAbout = () => {
-    if (router.pathname.includes("contact")) {
+    if (router.pathname.includes('contact')) {
       return (
         <Link
-          href="/#about"
-          className="hover:text-[#ECB5F5] transition-all duration-300"
+          href='/#about'
+          className='hover:text-[#ECB5F5] transition-all duration-300'
         >
           <button onClick={() => setMenuToggled(false)}>About</button>
         </Link>
-      );
+      )
     }
     return (
       <button
-        className="hover:text-[#ECB5F5] transition-all duration-300"
+        className='hover:text-[#ECB5F5] transition-all duration-300'
         onClick={() => {
           document
-            .querySelector("#about")
-            ?.scrollIntoView({ behavior: "smooth" });
-          setMenuToggled(false);
+            .querySelector('#about')
+            ?.scrollIntoView({ behavior: 'smooth' })
+          setMenuToggled(false)
         }}
       >
         About
       </button>
-    );
-  };
+    )
+  }
 
   return (
-    <header className="min-h-screen w-full relative mb-[10%] bg-gradient-primary">
-      <nav className="!bg-transparent w-full ml-auto flex font-Lexend pl-4 pr-6 py-2 text-white">
+    <header
+      className={cs('min-h-screen w-full relative mb-[10%]', styles.gradient, {
+        [styles.gradientActive]: isRainbowActive,
+      })}
+    >
+      <nav className='!bg-transparent w-full ml-auto flex font-Lexend pl-4 pr-6 py-2 text-white'>
         <Link
-          className="flex items-center lg:min-w-[600px] font-medium text-xl"
-          href="/"
+          className='flex items-center lg:min-w-[600px] font-medium text-xl'
+          href='/'
         >
-          <Image className="w-[15%] p-[2%]" src={logo} alt="LOGO" priority />
+          <Image className='w-[15%] p-[2%]' src={logo} alt='LOGO' priority />
           Immersive
         </Link>
         <button
-          className="items-left ml-auto lg:hidden"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className='items-left ml-auto lg:hidden'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
           onClick={() => setMenuToggled(!isMenuToggled)}
         >
           <div
-            className={cs("tham tham-e-squeeze tham-w-6", {
-              "tham-active": isMenuToggled,
+            className={cs('tham tham-e-squeeze tham-w-6', {
+              'tham-active': isMenuToggled,
             })}
           >
-            <div className="tham-box">
-              <div className="tham-inner bg-white/50" />
+            <div className='tham-box'>
+              <div className='tham-inner bg-white/50' />
             </div>
           </div>
         </button>
 
-        <ul className="max-lg:hidden ml-auto justify-end flex font-medium items-center gap-4">
+        <ul className='max-lg:hidden ml-auto justify-end flex font-medium items-center gap-4'>
           <li>{renderAbout()}</li>
           <li>
             <Link
-              href="/contact#contact"
-              className="hover:text-[#ECB5F5] transition-all duration-300"
+              href='/contact#contact'
+              className='hover:text-[#ECB5F5] transition-all duration-300'
             >
               <button onClick={() => setMenuToggled(false)}>Contact</button>
             </Link>
           </li>
-          <Flags />
+          <Flags onClickPride={onClickPride} />
         </ul>
       </nav>
       <nav
-        className={cs("text-white font-Lexend font-medium text-center", {
+        className={cs('text-white font-Lexend font-medium text-center', {
           hidden: !isMenuToggled,
         })}
       >
-        <ul className="py-8">
+        <ul className='py-8'>
           <li>{renderAbout()}</li>
         </ul>
         <ul>
           <li>
             <Link
-              href="/contact#contact"
-              className="hover:text-[#ECB5F5] transition-all duration-300"
+              href='/contact#contact'
+              className='hover:text-[#ECB5F5] transition-all duration-300'
             >
               <button onClick={() => setMenuToggled(false)}>Contact</button>
             </Link>
           </li>
-          <Flags />
+          <Flags onClickPride={onClickPride} />
         </ul>
       </nav>
-      <div className="w-full sm:max-w-[640] px-4 md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] 2xl:mx-w-[1536px] mx-auto">
+      <div className='w-full sm:max-w-[640] px-4 md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] 2xl:mx-w-[1536px] mx-auto'>
         <div
-          className={cs("flex lg:mt-[10%] flex-wrap pl-4", {
-            "mt-[10%]": !isMenuToggled,
+          className={cs('flex lg:mt-[10%] flex-wrap pl-4', {
+            'mt-[10%]': !isMenuToggled,
           })}
         >
-          <div className="basis-full lg:basis-1/3 px-4">
-            <div className="mr-[10%] text-white leading-6">
-              <h1 className="font-Dela text-3xl mb-4 lg:mb-3 max-lg:pt-12">
+          <div className='basis-full lg:basis-1/3 px-4'>
+            <div className='mr-[10%] text-white leading-6'>
+              <h1 className='font-Dela text-3xl mb-4 lg:mb-3 max-lg:pt-12'>
                 HUMAN-CENTERED AR/MR/VR
               </h1>
-              <p className="font-Lexend mb-4  font-medium">
+              <p className='font-Lexend mb-4  font-medium'>
                 Immersive solutions for you and your business
               </p>
               {renderButton()}
             </div>
           </div>
-          <div className="basis-full lg:basis-2/3 px-4">
-            <div className="text-center mt-[25%] lg:mt-[5%] ml-[10%] w-[90%]">
+          <div className='basis-full lg:basis-2/3 px-4'>
+            <div className='text-center mt-[25%] lg:mt-[5%] ml-[10%] w-[90%]'>
               <Image
                 src={headerImg}
-                className="lg:w-[80%] -scale-x-100"
-                alt="ar-img"
+                className='lg:w-[80%] -scale-x-100'
+                alt='ar-img'
                 priority
               />
             </div>
@@ -152,5 +163,5 @@ export const Header: React.FC = () => {
         </div>
       </div>
     </header>
-  );
-};
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-primary":
-          "linear-gradient(135deg, #291f3b 20%, #796c91 100%)",
+        'gradient-primary':
+          'linear-gradient(135deg, #291f3b 20%, #796c91 100%)',
+        'gradient-pride':
+          'linear-gradient(to right, rgb(237, 34, 36), rgb(243, 91, 34), rgb(249, 150, 33), rgb(245, 193, 30), rgb(241, 235, 27) 27%, rgb(241, 235, 27), rgb(241, 235, 27) 33%, rgb(99, 199, 32), rgb(12, 155, 73), rgb(33, 135, 141), rgb(57, 84, 165), rgb(97, 55, 155), rgb(147, 40, 142))',
       },
       fontFamily: {
-        Lexend: ["Lexend", ...defaultTheme.fontFamily.mono],
+        Lexend: ['Lexend', ...defaultTheme.fontFamily.mono],
         Dela: [
           '"Dela Gothic One", Helvetica, sans-serif',
           ...defaultTheme.fontFamily.mono,
@@ -21,5 +23,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-hamburgers")],
-};
+  plugins: [require('tailwind-hamburgers')],
+}
